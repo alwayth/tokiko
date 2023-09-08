59f42c35fd208f715c09063eaa7ad8a8b1065d7d
alwayth <ksiwt.jp@gmail.com>
2023-09-08 20:09:38 +0900

    :recycle: fix main.go.

--- main.go
+++ main.go
@@ -3,20 +3,20 @@ package main
 import (
 	"fmt"
 	"os"
+
 	"tokiko/cli"
 	"tokiko/generate"
 )
 
 func main() {
-	cli := cli.NewCLI(os.Stdin, os.Stdout)
-	input, output, err := cli.Run(os.Args)
+	c := cli.NewCLI(os.Stdin, os.Stdout)
+	input, output, err := c.Run(os.Args)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(0)
 	}
 
-	generator := generate.NewGenerator()
-	if err := generator.Generate(input, output); err != nil {
+	if err := generate.Generate(input, output); err != nil {
 		fmt.Println(err)
 		os.Exit(0)
 	}
